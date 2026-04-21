2863c8d507bd0b7c43d1e20c58d96f886b4703d6
AyuJuliana <ayujuliani349@gmail.com>
2026-04-21 15:28:31 +0800

    Initial Commit

---
+++ My Contacts Remix/My_Contacts_RemixApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_Contacts_RemixApp.swift
+//  My Contacts Remix
+//
+//  Created by Ni Komang Ayu Juliana on 20/04/26.
+//
+
+import SwiftUI
+
+@main
+struct My_Contacts_RemixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
